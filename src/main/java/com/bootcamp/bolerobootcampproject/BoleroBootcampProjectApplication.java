450a34d0686041afd8abe7d13aebd0a1f7910a6f
nish4nth-m <nishanth.murali@wisetechglobal.com>
2025-08-05 19:47:32 +0530

    first commit

---
+++ src/main/java/com/bootcamp/bolerobootcampproject/BoleroBootcampProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.bolerobootcampproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoleroBootcampProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoleroBootcampProjectApplication.class, args);
+	}
+
+}
